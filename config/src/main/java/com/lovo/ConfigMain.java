dcad5867228616a5cdd6ef377533eb2f231280b4
climber1982 <403783154@qq.com>
2019-09-19 10:43:20 +0800

    init

--- config/src/main/java/com/lovo/ConfigMain.java
+++ config/src/main/java/com/lovo/ConfigMain.java
@@ -1,16 +1,16 @@
-package com.lovo.client;
+package com.lovo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableDiscoveryClient
-public class MainOne {
+@EnableConfigServer
+public class ConfigMain {
     public static void main(String[] args) {
-
-        SpringApplication.run(MainOne.class);
+        SpringApplication.run(ConfigMain.class);
     }
 }
