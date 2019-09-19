7cc8a5437aec32fa118a1c2df01fac840e4fffa2
climber1982 <403783154@qq.com>
2019-09-19 11:31:19 +0800

    init

--- clientone/src/main/java/com/lovo/client/controller/RpcController.java
+++ clientone/src/main/java/com/lovo/client/controller/RpcController.java
@@ -2,6 +2,7 @@ package com.lovo.client.controller;
 
 import com.lovo.client.entity.UserEntity;
 import com.lovo.client.service.IUserService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -16,9 +17,11 @@ public class RpcController {
     public String rpcMessage(){
         return "hello cloud !!";
     }
-   @RequestMapping("user/{tag}")
-    public UserEntity getUser(@PathVariable int tag){
 
+   @RequestMapping("user/{tag}")
+   @HystrixCommand(fallbackMethod = "error")
+    public UserEntity getUser(@PathVariable int tag){
+         int i=1/0;
        UserEntity user=new UserEntity();
        if(tag==0){
            user.setUserName("赵云");
@@ -26,6 +29,13 @@ public class RpcController {
            user.setUserName("张飞");
        }
        return  user;
+    }
+
+    public UserEntity error(@PathVariable int tag){
+        System.out.println(tag);
+        UserEntity user=new UserEntity();
+        user.setUserName("error");
+        return user;
     }
     @RequestMapping("findUserByName")
     public UserEntity findUserByName(@RequestBody UserEntity userEntity){
--- clienttwo/src/main/java/com/lovo/cilent/ClientTwoMain.java
+++ clienttwo/src/main/java/com/lovo/cilent/ClientTwoMain.java
@@ -3,13 +3,16 @@ package com.lovo.cilent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
+
 public class ClientTwoMain {
 
     public static void main(String[] args) {
